f1fd687d2a82dcbcaf38e66d6c6c370f3e79afcc
ksks <ktsuzuki0@gmail.com>
2020-08-09 02:10:07 +0900

    feat: p2

---
+++ SolutionToProblem2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolutionToProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var input1 = new List<object>
+            {
+                "a", "b", "c"
+            };
+            var input2 = new List<object>
+            {
+                1, 2, 3
+            };
+
+            f(input1, input2).ForEach(n =>
+            {
+                Console.Write($"{n} ");
+            });
+        }
+
+        static List<object> f(in List<object> input1, in List<object> input2)
+        {
+            var result = new List<object>();
+            
+            for (var i = 0; i < input1.Count; i++)
+            { 
+                result.Add(input1[i]);
+                if (i < input2.Count)
+                {
+                    result.Add(input2[i]);
+                }
+            }
+
+            for (var diff = input2.Count - (input2.Count - input1.Count); diff < input2.Count; diff++)
+            {
+                result.Add(input2[diff]);
+            }
+            
+            return result;
+        }
+    }
+}
