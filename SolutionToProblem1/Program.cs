41a252589b8e2481bef7b950dc0e0f1ca74530bb
ksks <ktsuzuki0@gmail.com>
2020-08-09 01:48:49 +0900

    feat: P1

--- SolutionToProblem1/Program.cs
+++ SolutionToProblem1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SolutionToProblem1
 {
@@ -7,6 +8,48 @@ namespace SolutionToProblem1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var input = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+            Console.WriteLine($"For: {For(input)}");
+            Console.WriteLine($"While: {While(input)}");
+            Console.WriteLine($"Recursive: {Recursive(input)}");
+        }
+
+        static int For(in List<int> input)
+        {
+            var result = 0;
+            for (var i = 0; i < input.Count; i++)
+            {
+                result += input[i];
+            }
+            return result;
+        }
+        
+        static int While(in List<int> input)
+        {
+            var result = 0;
+            if (input.Count == 0) return result;
+
+            var index = 0;
+            while (index < input.Count)
+            {
+                result += input[index];
+                ++index;
+            }
+
+            return result;
+        }
+
+        static int Recursive(in List<int> input)
+        {
+            return R(input, 0);
+        }
+
+        static int R(in List<int> input, in int index)
+        {
+            return index >= input.Count ? 0 : input[index] + R(input, index + 1);
         }
     }
 }
