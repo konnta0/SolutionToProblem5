e7c3800989f78a289cdb79b314d4931f1e2d64b0
ksks <ktsuzuki0@gmail.com>
2020-08-09 02:34:47 +0900

    feat: p3

---
+++ SolutionToProblem3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolutionToProblem3
+{
+    class Program
+    {
+        private static Dictionary<long, long> cache = new Dictionary<long, long>();
+        static void Main(string[] args)
+        {
+            var n = 100;
+            var builder = new StringBuilder();
+            for (var i = 0; i < n; i++)
+            {
+                builder.AppendLine($"{f(i)}");
+            }
+            Console.WriteLine(builder.ToString());
+        }
+
+        static long f(long n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (!cache.ContainsKey(n))
+            {
+                cache[n] = f(n - 1) + f(n - 2);
+            }
+            return cache[n];
+        }
+    }
+}
