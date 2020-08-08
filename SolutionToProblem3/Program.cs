960489057e25155b1b52b566a0a3e5d8e3b3545a
ksks <ktsuzuki0@gmail.com>
2020-08-09 02:46:54 +0900

    fix: Another pattern

--- SolutionToProblem3/Program.cs
+++ SolutionToProblem3/Program.cs
@@ -9,13 +9,17 @@ namespace SolutionToProblem3
         private static Dictionary<long, long> cache = new Dictionary<long, long>();
         static void Main(string[] args)
         {
-            var n = 100;
+            var n = 10;
             var builder = new StringBuilder();
             for (var i = 0; i < n; i++)
             {
                 builder.AppendLine($"{f(i)}");
             }
             Console.WriteLine(builder.ToString());
+
+            var l = new List<long> {0, 1};
+            f2(ref l, n);
+            l.ForEach(Console.WriteLine);
         }
 
         static long f(long n)
@@ -27,6 +31,13 @@ namespace SolutionToProblem3
                 cache[n] = f(n - 1) + f(n - 2);
             }
             return cache[n];
+        }
+
+        static void f2(ref List<long> ret, int n)
+        {
+            if (ret.Count >= n) return;
+            ret.Add(ret[ret.Count - 2] + ret[ret.Count - 1]);
+            f2(ref ret, n);
         }
     }
 }
